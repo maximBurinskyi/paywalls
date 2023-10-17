61b884bb43c6432c683b32875bcc9571e569d828
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-10-17 10:57:04 +0300

    added route, controller, model data

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   LightModeOutlined,
   DarkModeOutlined,
@@ -12,15 +12,73 @@ import { useDispatch } from 'react-redux';
 import { setMode } from 'state';
 import {
   AppBar,
+  Box,
+  Button,
   IconButton,
   InputBase,
+  Menu,
+  MenuItem,
   Toolbar,
+  Typography,
   useTheme,
 } from '@mui/material';
+import profileImage from '../assets/profile.jpg';
 
-function Navbar({ isSidebarOpen, setIsSidebarOpen }) {
+import axios from 'axios';
+import { usePlaidLink } from 'react-plaid-link';
+
+// Project 1 (working baseURL)
+//axios.defaults.baseURL = 'http://localhost:5001';
+
+// Project 1 (additional component)
+// function PlaidAuth({ publicToken }) {
+//   useEffect(() => {
+//     async function fetchData() {
+//       let accessToken = await axios.post('/exchange_public_token', {
+//         public_token: publicToken,
+//       });
+//       console.log('accessToken', accessToken.data);
+//       const auth = await axios.post('/auth', {
+//         access_token: accessToken.data.accessToken,
+//       });
+//       console.log('auth data', auth.data);
+//     }
+//     fetchData();
+//   }, []);
+//   return <span>{publicToken}</span>;
+// }
+
+function Navbar({ user, isSidebarOpen, setIsSidebarOpen }) {
   const dispatch = useDispatch();
   const theme = useTheme();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
+
+  const [linkToken, setLinkToken] = useState();
+  const [publicToken, setPublicToken] = useState();
+
+  // Project 1 (working useEffect)
+  // useEffect(() => {
+  //   async function fetch() {
+  //     const response = await axios.post('/create_link_token');
+  //     setLinkToken(response.data.link_token);
+  //     console.log('response', response.data);
+  //   }
+
+  //   fetch();
+  // }, []);
+
+  // Project 1 (usePlaidLink)
+  // const { open, ready } = usePlaidLink({
+  //   token: linkToken,
+  //   onSuccess: (public_token, metadata) => {
+  //     setPublicToken(public_token);
+  //     console.log('success', public_token, metadata);
+  //     // send public_token to server
+  //   },
+  // });
 
   return (
     <AppBar
@@ -60,6 +118,62 @@ function Navbar({ isSidebarOpen, setIsSidebarOpen }) {
           <IconButton>
             <SettingsOutlined sx={{ fontSize: '25px' }} />
           </IconButton>
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                textTransfor: 'none',
+                gap: '1rem',
+              }}
+            >
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="32px"
+                width="32x"
+                borderRadius="50%"
+                sx={{ objectFit: 'cover' }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.85rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user?.name}
+                </Typography>
+                <Typography
+                  fontSize="0.75rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user?.occupation}
+                </Typography>
+              </Box>
+              <ArrowDropDownOutlined
+                sx={{ color: theme.palette.secondary[300], fontSize: '25px' }}
+              />
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              open={isOpen}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+            >
+              <MenuItem onClick={handleClose}>Log Out</MenuItem>
+            </Menu>
+          </FlexBetween>
+          {/* Project 1 button */}
+          {/* {publicToken ? (
+            <PlaidAuth publicToken={publicToken} />
+          ) : (
+            <button onClick={() => open()} disabled={!ready}>
+              Connect a bank account
+            </button>
+          )} */}
         </FlexBetween>
       </Toolbar>
     </AppBar>
--- server/index.js
+++ server/index.js
@@ -9,8 +9,27 @@ import clientRoutes from './routes/client.js';
 import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
+import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
+
+//data imports
+import User from './models/User.js';
+import { dataUser } from './data/index.js';
 
 dotenv.config();
+
+// Project 1 (configuration)
+// const configuration = new Configuration({
+//   basePath: PlaidEnvironments.sandbox,
+//   baseOptions: {
+//     headers: {
+//       'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+//       'PLAID-SECRET': process.env.PLAID_SECRET,
+//     },
+//   },
+// });
+
+// const plaidClient = new PlaidApi(configuration);
+
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -26,6 +45,62 @@ app.use('/general', generalRoutes);
 app.use('/management', managementRoutes);
 app.use('/sales', salesRoutes);
 
+// Project 1 (1 endpoint)
+// app.post('/create_link_token', async function (request, response) {
+//   const plaidRequest = {
+//     user: {
+//       // This should correspond to a unique id for the current user.
+//       client_user_id: 'user',
+//     },
+//     client_name: 'Plaid Test App',
+//     products: ['auth'],
+//     language: 'en',
+//     redirect_uri: 'http://localhost:3001/',
+//     country_codes: ['US'],
+//   };
+//   try {
+//     const createTokenResponse = await plaidClient.linkTokenCreate(plaidRequest);
+//     response.json(createTokenResponse.data);
+//   } catch (error) {
+//     response.status(500).send('failure');
+//   }
+// });
+
+// Project 1 (third endpoint)
+// app.post('/auth', async function (request, response) {
+//   try {
+//     const access_token = request.body.access_token;
+//     const plaidRequest = {
+//       access_token: access_token,
+//     };
+
+//     const plaidResponse = await plaidClient.authGet(plaidRequest);
+//     response.json(plaidResponse.data);
+//   } catch (e) {
+//     response.status(500).send('failed');
+//   }
+// });
+
+// Project 1 (second endpoint)
+// app.post('/exchange_public_token', async function (request, response, next) {
+//   const publicToken = request.body.public_token;
+//   try {
+//     const plaidResponse = await plaidClient.itemPublicTokenExchange({
+//       public_token: publicToken,
+//     });
+
+//     // These values should be saved to a persistent database and
+//     // associated with the currently signed-in user
+//     const accessToken = plaidResponse.data.access_token;
+//     //const itemID = response.data.item_id;
+
+//     response.json({ accessToken });
+//   } catch (error) {
+//     // handle error
+//     response.status(500).send('failed');
+//   }
+// });
+
 //Mongoose setup
 const PORT = process.env.PORT || 9000;
 
@@ -36,5 +111,12 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
+
+// Project 1 (working server)
+// app.listen(PORT, () => {
+//   console.log('server has started');
+// });
