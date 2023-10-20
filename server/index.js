2c1df094e9a4b73ce5dadf501159a20d773793cb
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-10-20 23:25:33 +0300

    builded overview page

--- server/index.js
+++ server/index.js
@@ -16,11 +16,13 @@ import User from './models/User.js';
 import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
 import Transaction from './models/Transaction.js';
+import OverallStat from './models/OverallStat.js';
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
+  dataOverallStat,
 } from './data/index.js';
 
 dotenv.config();
@@ -124,6 +126,7 @@ mongoose
     // ProductStat.insertMany(dataProductStat);
     // User.insertMany(dataUser);
     // Transaction.insertMany(dataTransaction)
+    // OverallStat.insertMany(dataOverallStat);
   })
   .catch((error) => console.log(`${error} did not connect`));
 
